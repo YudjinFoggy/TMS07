54993036f11908bd994f0bf26c7e52525b2c2862
DESKTOP-NN2F9VB\Yume <enchanteur@mail.ru>
2021-04-12 00:28:33 +0300

    Button "homework_1" switches to new activity "HW1Activity"

---
+++ app/src/main/java/com/example/tms07/HW1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.tms07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HW1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hw1)
+    }
+}
